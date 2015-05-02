ddfd776130fa296610c4285de22ec9b4bc363f6b
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2015-05-02 09:41:59 +0900

    New: copy_to_www.sh

---
+++ script/copy_to_www.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+PARRO_HOME=$HOME/work/parro_typing
+
+cp $PARRO_HOME/www/about.php $HOME/www/parro_typing/
+cp $PARRO_HOME/www/index.php $HOME/www/parro_typing/
+cp $PARRO_HOME/www/link.php $HOME/www/parro_typing/
+cp $PARRO_HOME/www/login.php $HOME/www/parro_typing/
+cp $PARRO_HOME/www/login-result.php $HOME/www/parro_typing/
+cp $PARRO_HOME/www/signup.php $HOME/www/parro_typing/
+cp $PARRO_HOME/www/signup-result.php $HOME/www/parro_typing/
+
+cp $PARRO_HOME/www/app/bookmark.php $HOME/www/parro_typing/app/
+cp $PARRO_HOME/www/app/config.php $HOME/www/parro_typing/app/
+cp $PARRO_HOME/www/app/play-game.php $HOME/www/parro_typing/app/
+cp $PARRO_HOME/www/app/index.php $HOME/www/parro_typing/app/
+cp $PARRO_HOME/www/app/main.css $HOME/www/parro_typing/app/
+cp $PARRO_HOME/www/app/make.php $HOME/www/parro_typing/app/
